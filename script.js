2214de28ee81dd733aa0909effd6631fb185484e
MaivaC <kamgamaiva@gmail.com>
2023-01-04 05:04:26 -0300

    change weather icon

--- script.js
+++ script.js
@@ -17,35 +17,37 @@ h3.innerHTML = `${day} ${date},${hours}:${minutes}`;
 
 
 function showTemperature(response) {
-  console.log(response.data);
+ 
+  let temperatureElement = document.querySelector("#temperature");
+   let cityName=document.querySelector("h2");
+    let descriptionElement=document.querySelector("#description");
+     let humidityElement=document.querySelector("#humidity");
+     let windElement=document.querySelector("#wind");
+     let dateElement=document.querySelector("#date");
+      let iconElement=document.querySelector("#icon");
+     
   let city = response.data.name;
-  let cityName=document.querySelector("h2");
+ 
   cityName.innerHTML=`${city}`;
   let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperature");
+  
   temperatureElement.innerHTML = `${temperature}`;
+  descriptionElement.innerHTML= response.data.weather[0].description;
+   humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+   dateElement.innerHTML =formalDate(response.data.dt*1000);
+   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+   iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
-function search(event) {
-  event.preventDefault();
-  let input = document.querySelector("form input");
-  searchCity(input.value);
-}
 
-function searchCity(city) {
+
+
   let apiKey = "1b903b13fd4caf1f82d1ab59d8fe0b9f";
+  let city="Paris";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
-}
 
-let form = document.querySelector("form");
-form.addEventListener("submit", search);
-
-function getLocation(location) {
-  console.log(location);
-}
-
-navigator.geolocation.getCurrentPosition(getLocation);
 
 
 
