52edfb6b31187fa72c8d10df257e0960ccaced36
MaivaC <kamgamaiva@gmail.com>
2023-01-07 04:42:11 -0300

    updated weather app. The date and place now displays after being searched for

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let h3 = document.querySelector("h3");
 let now = new Date();
 let date = now.getDate();
@@ -17,76 +16,75 @@ h3.innerHTML = `${day} ${date},${hours}:${minutes}`;
 
 
 function showTemperature(response) {
- 
+
   let temperatureElement = document.querySelector("#temperature");
-   let cityName=document.querySelector("h2");
-    let descriptionElement=document.querySelector("#description");
-     let humidityElement=document.querySelector("#humidity");
-     let windElement=document.querySelector("#wind");
-     let dateElement=document.querySelector("#date");
-      let iconElement=document.querySelector("#icon");
-      celsuisTempertaure=response.data.main.temp;
+  let cityName = document.querySelector("h2");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  celsuisTempertaure = response.data.main.temp;
 
 
 
   let city = response.data.name;
- cityName.innerHTML=`${city}`;
+  cityName.innerHTML = `${city}`;
   let temperature = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = `${temperature}`;
-  descriptionElement.innerHTML= response.data.weather[0].description;
-   humidityElement.innerHTML=response.data.main.humidity;
-    windElement.innerHTML=Math.round(response.data.wind.speed);
-   dateElement.innerHTML =formalDate(response.data.dt*1000);
-   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-   iconElement.setAttribute("alt",response.data.weather[0].description);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
-function search(city){
-let apiKey = "1b903b13fd4caf1f82d1ab59d8fe0b9f";
-  let city="Paris";
+function search(city) {
+  let apiKey = "1b903b13fd4caf1f82d1ab59d8fe0b9f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 
 }
- function handleSubmit(event){
-    event.preventDefault();
-    let cityInputElement=document.querySelector("#city-input");
-    search(cityInputElement.value);
- }
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
- function displayFahenreitTemperature(event){
+function displayFahenreitTemperature(event) {
   event.preventdefault();
-  let fahenreitTemperature=( celsuisTempertaure*9)/5 +32;
+  let fahenreitTemperature = (celsuisTempertaure * 9) / 5 + 32;
   //remove the active class from celsuis link
   celsuisLink.classList.remove("active");
   fahenreitLink.classList.add("active");
-  let temperatureElement=document.querySelector("#temperature");
-temperatureElement.innerHTML=Math.round(fahenreitTemperature);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahenreitTemperature);
   alert("link clicked");
 }
 
-function displayCelsuisTemperature(event){
+function displayCelsuisTemperature(event) {
   event.preventDefault();
-  let temperatureElement=document.querySelector("#temperature");
+  let temperatureElement = document.querySelector("#temperature");
   //add the active class from celsuis link
   celsuisLink.classList.add("active");
   fahenreitLink.classList.remove("active");
-  temperatureElement.innerHTML=Math.round(celsuisTemperature);
+  temperatureElement.innerHTML = Math.round(celsuisTemperature);
 }
 
- let celsuisTempertaure=null;
+let celsuisTempertaure = null;
 
 
-  let form=document.querySelector("#search-form");
-  form.addEventListener("submit", handleSubmit);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 
-  let fahenreitLink=document.querySelector("#Fahenreit-link");
-fahenreitLink.addEventListener("click",displayFahenreitTemperature);
+let fahenreitLink = document.querySelector("#Fahenreit-link");
+fahenreitLink.addEventListener("click", displayFahenreitTemperature);
 
-let celsuisLink=documet.querySelector("#celsuis-link");
-celsuisLink.addEventListener("click",displayCelsuisTemperature);
+let celsuisLink = document.querySelector("#celsuis-link");
+celsuisLink.addEventListener("click", displayCelsuisTemperature);
 
- search("Paris");
+search("Paris");
+
